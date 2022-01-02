e09a36e6b3722dc3b0374ba1ffafae6f34ef6512
Michelle Wang <tianw0420@gmail.com>
2022-01-02 11:38:06 -0800

    tasks 2-4

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ user.surname = "Smith";
 user.name = "Pete";  
 delete user.name; 
 
+/* Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false */ 
+
+function isEmpty(obj){ 
+    for (let key in obj){
+      return false; 
+    }
+    return true; 
+  }
+
+/*Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+If salaries is empty, then the result must be 0.*/ 
+let sum = 0; 
+for (let key in salaries){
+    sum += salaries[key]; 
+}
+
+/*Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.*/ 
+function multiplyNumeric(obj) {
+    for (let key in obj){
+      if (typeof obj[key] == "number"){
+        obj[key] = obj[key] * 2; 
+      }
+    }
+  }
+
